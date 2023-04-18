ea7e74205faf10ed176ea0138803caff35dad6a1
K Davidson <kreatord@gmail.com>
2023-04-18 15:34:28 +0100

    feat: add tooltips to markers
    updated styling and cleaned code

--- script.js
+++ script.js
@@ -1,193 +1,220 @@
-var currentNearbyStops = [];
-var currentStop = null;
-var intervalId;
+const hideLoading = () => (loading.style.visibility = "hidden");
+const showLoading = () => (loading.style.visibility = "visible");
 
-var map = L.map("map", {
-    center: [51.507209, -0.127614],
-    minZoom: 11,
-    maxZoom: 17,
-    zoom: 17,
+const clearStopsList = () => {
+  document.getElementById("stopsList").innerHTML = "";
+};
+
+const addStopToStopsList = (index, indicator, name, towards, lines) => {
+  const stopsList = document.getElementById("stopsList");
+  const linesHTML = lines
+    .map((line) => `<div class="line"> ${line.name} </div>`)
+    .join("");
+
+  stopsList.innerHTML += `
+      <div class="stop" onclick="setCurrentStop(${index})">
+          <div class="header">
+              <div class="stopLetter">${indicator.replace("->", "\u2B95")}</div>
+              <div class="info">
+                  <div class="stopName"><span>${name}</span></div>
+                  <div class="towards">${towards}</div>
+              </div>
+          </div>
+          <div class="lines">
+              ${linesHTML || `<div class="line"> No lines </div>`}
+          </div>
+      </div>
+  `;
+};
+
+const setNearbyStops = () => {
+  clearStopsList();
+  currentNearbyStops.map((stop, index) => {
+    addStopToStopsList(
+      index,
+      stop.indicator,
+      stop.name,
+      stop.towards,
+      stop.lines
+    );
+    addMarker(index, stop.lat, stop.lng, stop.mode, stop.lines);
+  });
+};
+
+const addNearbyStopsToMap = (lat, lng) => {
+  showLoading();
+
+  getNearbyStops(lat, lng).then((nearbyStops) => {
+    currentNearbyStops = nearbyStops;
+    setNearbyStops();
+    hideLoading();
+  });
+};
+
+const unsetCurrentStop = () => {
+  if (intervalId !== null) clearInterval(intervalId);
+  currentStop = null;
+  document.getElementById("stopsList").style.display = "block";
+  document.getElementById("departures").style.display = "none";
+};
+
+const addDeparturesToDeparturesList = (name, destination, mins) => {
+  const departures = document.getElementById("departures");
+  departures.innerHTML += `
+      <div class="departure">
+          <div class="header">
+              <div class="info">
+                  <div class="nameContainer">
+                      <div class="name">${name}</div><span></span>
+                  </div>
+                  <div class="destination">${destination}</div>
+              </div>
+          </div>
+          <div class="mins">
+              ${mins}
+          </div>
+      </div>
+  `;
+};
+
+const setCurrentStop = (index) => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    hideLoading();
+  }
+  showLoading();
+  currentStop = currentNearbyStops[index];
+  document.getElementById("stopsList").style.display = "none";
+  document.getElementById("departures").style.display = "block";
+
+  const departures = document.getElementById("departures");
+  const linesHTML =
+    currentStop && currentStop.lines
+      ? currentStop.lines
+          .map((line) => `<div class="line"> ${line.name} </div>`)
+          .join("")
+      : `<div class="line"> No lines </div>`;
+
+  departures.innerHTML = `
+      <div class="stop">
+          <div class="header">
+              <div class="back" onclick="unsetCurrentStop()">&#10094;</div>
+              <div class="info">
+                  <div class="stopName"><span>${currentStop.name}</span></div>
+                  <div class="towards">${currentStop.towards}</div>
+              </div>
+          </div>
+          <div class="lines">
+              ${linesHTML}
+          </div>
+      </div>
+  `;
+
+  getStopDepartures(currentStop).then((stopDepartures) => {
+    stopDepartures.map((stop) => {
+      addDeparturesToDeparturesList(
+        stop.name,
+        stop.destination,
+        stop.mins == 0 ? "due" : stop.mins + " mins"
+      );
+    });
+    hideLoading();
+  });
+
+  intervalId = setTimeout(() => setCurrentStop(index), 5000);
+};
+
+const showPosition = (position) => {
+  addMarker(-1, position.coords.latitude, position.coords.longitude, 2);
+};
+
+const getUserLocation = () => {
+  if (navigator.geolocation) {
+    showLoading();
+    navigator.geolocation.getCurrentPosition((position) => {
+      unsetCurrentStop();
+      map.setView(
+        [position.coords.latitude, position.coords.longitude],
+        map.zoom
+      );
+      showPosition(position);
+      addNearbyStopsToMap(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+};
+
+const addMarker = (index, lat, lng, icon, lines = []) => {
+  let iconUrl = "imgs/busHighlight.png";
+
+  switch (icon) {
+    default:
+      iconUrl = "imgs/busHighlight.png";
+      break;
+    case 1:
+      iconUrl = "imgs/trainHighlight.png";
+      break;
+    case 2:
+      iconUrl = "imgs/userLocationMarker.png";
+      break;
+  }
+
+  if (map.getBounds().contains([lat, lng])) {
+    const century21icon = L.icon({
+      iconUrl,
+      iconSize: [30, 30],
+    });
+
+    L.marker([lat, lng], {
+      title: "",
+      icon: century21icon,
+    })
+      .addTo(map)
+      .on("click", (e) => setCurrentStop(index))
+      .bindTooltip(
+        `<div class="lines">${
+          lines
+            .map((line) => `<div class="line"> ${line.name} </div>`)
+            .join("") || `<div class="line"> No lines </div>`
+        }</div>`,
+        {
+          direction: "top",
+          opacity: 0.8,
+        }
+      );
+  }
+};
+
+let currentNearbyStops = [];
+let currentStop = null;
+let intervalId;
+
+const loading = document.getElementsByClassName("loading")[0];
+
+const map = L.map("map", {
+  center: [51.507209, -0.127614],
+  minZoom: 11,
+  maxZoom: 17,
+  zoom: 17,
 });
 
 const mapBounds = L.latLngBounds([
-    [51.944653, -0.756512],
-    [51.031025, 0.721172],
+  [51.944653, -0.756512],
+  [51.031025, 0.721172],
 ]);
 map.fitBounds(mapBounds);
 
 map.setMaxBounds(map.getBounds());
 
 const tiles = new L.tileLayer(
-    "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
-    {
-        attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }
+  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }
 ).addTo(map);
 
 map.setView(map.getCenter(), 17);
 
-console.log(map.getCenter());
-const addNearbyStopsToMap = () => {
-    getNearbyStops(map.getCenter().lat, map.getCenter().lng).then(
-        (nearbyStops) => {
-            currentNearbyStops = nearbyStops;
-            setNearbyStops();
-        }
-    );
-};
-
-addNearbyStopsToMap();
-
-const setNearbyStops = () => {
-    clearStopsList();
-    currentNearbyStops.map((stop, index) => {
-        addStopToStopsList(
-            index,
-            stop.indicator,
-            stop.name,
-            stop.towards,
-            stop.lines
-        );
-        addStopMarker(index, stop.lat, stop.lng, stop.mode);
-    });
-};
-
-const clearStopsList = () => {
-    var stopsList = document.getElementById("stopsList");
-    stopsList.innerHTML = "";
-};
-
-const unsetCurrentStop = () => {
-    intervalId == null ? null : clearInterval(intervalId);
-    currentStop = null;
-    document.getElementById("stopsList").style.display = "block";
-    document.getElementById("departures").style.display = "none";
-};
-
-const setCurrentStop = (index) => {
-    if (intervalId) {
-        clearInterval(intervalId);
-    }
-    console.log("updated");
-    currentStop = currentNearbyStops[index];
-    document.getElementById("stopsList").style.display = "none";
-    document.getElementById("departures").style.display = "block";
-
-    const departures = document.getElementById("departures");
-    var linesHTML = "";
-    console.log(currentStop);
-    currentStop.lines.forEach((line) => {
-        linesHTML += `<div class="line"> ${line.name} </div>`;
-    });
-    departures.innerHTML = `
-        <div class="stop">
-            <div class="header">
-                <div class="back" onclick="unsetCurrentStop()">&#10094;</div>
-                <div class="info">
-                    <div class="stopName"><span>${currentStop.name}</span></div>
-                    <div class="towards">${currentStop.towards}</div>
-                </div>
-            </div>
-            <div class="lines">
-                ${linesHTML}
-            </div>
-        </div>
-    `;
-
-    getStopDepartures(currentStop).then((stopDepartures) => {
-        stopDepartures.map((stop) => {
-            addDeparturesToDeparturesList(
-                stop.name,
-                stop.destination,
-                stop.mins == 0 ? "due" : stop.mins + " mins"
-            );
-        });
-    });
-
-    intervalId = setTimeout(setCurrentStop, 5000);
-};
-
-const addDeparturesToDeparturesList = (name, destination, mins) => {
-    const departures = document.getElementById("departures");
-    departures.innerHTML += `
-        <div class="departure">
-            <div class="header">
-                <div class="info">
-                    <div class="nameContainer">
-                        <div class="name">${name}</div><span></span>
-                    </div>
-                    <div class="destination">${destination}</div>
-                </div>
-            </div>
-            <div class="mins">
-                ${mins}
-            </div>
-        </div>
-    `;
-};
-
-const addStopToStopsList = (index, indicator, name, towards, lines) => {
-    const stopsList = document.getElementById("stopsList");
-    var linesHTML = "";
-    lines.forEach((line) => {
-        linesHTML += `<div class="line"> ${line.name} </div>`;
-    });
-    stopsList.innerHTML += `
-        <div class="stop" onclick="setCurrentStop(${index})">
-            <div class="header">
-                <div class="stopLetter">${indicator}</div>
-                <div class="info">
-                    <div class="stopName"><span>${name}</span></div>
-                    <div class="towards">${towards}</div>
-                </div>
-            </div>
-            <div class="lines">
-                ${linesHTML}
-            </div>
-        </div>
-    `;
-};
-
-const getUserLocation = () => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            unsetCurrentStop();
-            map.setView(
-                [position.coords.latitude, position.coords.longitude],
-                map.zoom
-            );
-            addNearbyStopsToMap();
-        });
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-};
-
-const showPosition = (position) => {
-    addMarker(51.507209, -0.127614);
-};
-
-const addStopMarker = (index, lat, lng, mode) => {
-    addMarker(index, lat, lng, mode);
-};
-
-const addMarker = (index, lat, lng, icon) => {
-    if (map.getBounds().contains([lat, lng])) {
-        var century21icon = L.icon({
-            iconUrl:
-                icon == 0 ? "imgs/busHighlight.png" : "imgs/trainHighlight.png",
-            iconSize: [30, 30],
-        });
-        var marker = L.marker([lat, lng], {
-            title: "",
-            icon: century21icon,
-        });
-
-        marker.addTo(map).on("click", (e) => setCurrentStop(index)); /*
-            .bindPopup(
-                "<p1><b>The White House</b><br>Landmark, historic home & office of the United States president, with tours for visitors.</p1>"
-            )
-            .openPopup();*/
-    }
-};
+addNearbyStopsToMap(map.getCenter().lat, map.getCenter().lng);
--- tflApiWrapper.js
+++ tflApiWrapper.js
@@ -1,68 +1,66 @@
 const getNearbyStops = async (lat, lon) => {
-    var endpointUrl = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=TransportInterchange,NaptanMetroStation,NaptanRailStation,NaptanBusCoachStation,NaptanFerryPort,NaptanPublicBusCoachTram&modes=tube,bus,coach,overground,dlr,cable-car,national-rail,river-bus,river-tour,tram&radius=1000`;
-    var response = await fetch(endpointUrl);
-    var json = await response.json();
+  var endpointUrl = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=TransportInterchange,NaptanMetroStation,NaptanRailStation,NaptanBusCoachStation,NaptanFerryPort,NaptanPublicBusCoachTram&modes=tube,bus,coach,overground,dlr,cable-car,national-rail,river-bus,river-tour,tram&radius=1000`;
+  var response = await fetch(endpointUrl);
+  var json = await response.json();
 
-    return json.stopPoints.map((e) => {
-        //console.log(e);
-        var mode = 0;
-        if (
-            e.modes.includes("dlr") ||
-            e.modes.includes("national-rail") ||
-            e.modes.includes("overground") ||
-            e.modes.includes("tube")
-        ) {
-            mode = 1;
-        }
-        var towards = "";
+  return json.stopPoints.map((e) => {
+    //console.log(e);
+    var mode = 0;
+    if (
+      e.modes.includes("dlr") ||
+      e.modes.includes("national-rail") ||
+      e.modes.includes("overground") ||
+      e.modes.includes("tube")
+    ) {
+      mode = 1;
+    }
+    var towards = "";
 
-        var towards = (
-            e.additionalProperties.find((e) => e.key == "Towards") || {
-                value: "",
-            }
-        ).value;
-        if (towards) {
-            towards = "towards " + towards;
-        }
-        return {
-            lat: e.lat,
-            lng: e.lon,
-            indicator:
-                "indicator" in e
-                    ? e.indicator
-                          .replace("Stop ", "")
-                          .replace("-bound", "")
-                          .replace("Stand ", "")
-                          .replace("Stop", "")
-                    : "-",
-            id: e.id,
-            towards: towards,
-            naptanId: e.naptanId,
-            name: e.commonName,
-            lines: e.lines.map((e) => {
-                return { name: e.name, id: e.id };
-            }),
-            mode: mode,
-        };
-    });
+    var towards = (
+      e.additionalProperties.find((e) => e.key == "Towards") || {
+        value: "",
+      }
+    ).value;
+    if (towards) {
+      towards = "towards " + towards;
+    }
+    return {
+      lat: e.lat,
+      lng: e.lon,
+      indicator:
+        "indicator" in e
+          ? e.indicator
+              .replace("Stop ", "")
+              .replace("-bound", "")
+              .replace("Stand ", "")
+              .replace("Stop", "")
+          : "-",
+      id: e.id,
+      towards: towards,
+      naptanId: e.naptanId,
+      name: e.commonName,
+      lines: e.lines.map((e) => {
+        return { name: e.name, id: e.id };
+      }),
+      mode: mode,
+    };
+  });
 };
 
 const getStopDepartures = async (stop) => {
-    var endpointUrl = `https://api.tfl.gov.uk/StopPoint/${stop.naptanId}/Arrivals`;
-    var response = await fetch(endpointUrl);
-    var json = await response.json();
+  var endpointUrl = `https://api.tfl.gov.uk/StopPoint/${stop.naptanId}/Arrivals`;
+  var response = await fetch(endpointUrl);
+  var json = await response.json();
 
-    var stopDepartures = json.map((e) => {
-        return {
-            name: e.lineName,
-            destination: e.destinationName,
-            mins: Math.round(
-                (new Date(e.expectedArrival) - Date.now()) / 60000
-            ),
-        };
-    });
-    stopDepartures.sort((a, b) => a.mins - b.mins);
-    console.log(stopDepartures);
+  var stopDepartures = json.map((e) => {
+    return {
+      name: e.lineName,
+      destination: e.destinationName,
+      mins: Math.round((new Date(e.expectedArrival) - Date.now()) / 60000),
+    };
+  });
+  stopDepartures.sort((a, b) => a.mins - b.mins);
+  console.log(stopDepartures);
 
-    return stopDepartures;
+  return stopDepartures;
 };
